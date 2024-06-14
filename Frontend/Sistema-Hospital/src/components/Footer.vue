0fa6e617d589bbb2303d59e5760e75882b941205
carlosM18-max <carloshernandez74199@gmail.com>
2024-06-14 13:24:07 -0600

    coreccion

--- Frontend/Sistema-Hospital/src/components/Footer.vue
+++ Frontend/Sistema-Hospital/src/components/Footer.vue
@@ -1,5 +1,5 @@
 <template>
-<footer class="bg-white dark:bg-gray-900 flex flex-col justify-between fixed inset-x-0">
+<footer class="bg-white dark:bg-gray-900 flex flex-col justify-between fixed inset-x-0 ">
     <div class="mx-auto w-full max-w-screen-xl p-4 py-6 lg:py-8">
         <div class="md:flex md:justify-between">
           <div class="mb-6 md:mb-0">
